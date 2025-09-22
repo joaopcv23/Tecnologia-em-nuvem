a2f1bcce0ee2bfaaacc9648d80c846785394f0e5
Joao Pedro Cardoso Venancio <163864247+joaopcv23@users.noreply.github.com>
2025-09-22 07:42:22 -0300

    Update index.php

--- app3/index.php
+++ app3/index.php
@@ -1,1 +1,23 @@
+<?php
 
+//Função para converter uma string para binário
+function stringParaBinario($string) {
+    //A Função ord pega valor ASCII de cada caractere
+    //A função decbin converte o valor ASCII em binário
+    $binario = '';
+    foreach(str_split($string) as $char){
+        //Converte o caractere para o valor binário de 8 bits
+        $binario .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . '';
+    }
+    return $binario;
+}
+
+//Exemplo de uso
+$frase = "João Pedro Cardoso Venancio";
+$fraseBinario = stringParaBinario ($frase);
+
+//Exibindo a frase original e sua versão binaria
+echo "Frase original: " . $frase . "<br>";
+echo "Frase em binário: " . $fraseBinario;
+
+?>
